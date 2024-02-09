d54718503112e44c6ff50c38d371c36674a6d021
SasithDuleepa <duleepajayathissa@gmail.com>
2024-02-09 16:47:50 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
 import './App.css';
 import { BrowserRouter as Router, Route,Routes, BrowserRouter } from "react-router-dom";
 
-import CreateNewRawItem from './inventory/pages/create new raw item/CreateNewRawItem';
-import CreateNewProduct from './inventory/pages/create new product/CreateNewProduct';
-import ProductView from './inventory/pages/product view/ProductView';
-import Customer from './inventory/pages/customer/Customer';
-import AddCustomer from './inventory/components/add customer/AddCustomer';
-import Supplier from './inventory/pages/supplier/Supplier';
-import AddSupplier from './inventory/components/add supplier/AddSupplier';
-
-import SupplierFilter from './inventory/components/supplier filter/SupplierFilter';
-
-import PurchaseRaw from './inventory/pages/Raw items/purchase raw/PurchaseRaw'
-import PurchaseRawStore from './inventory/pages/Raw items/purchase Raw Store/PurchaseRawStore';
-import PurchaseRawLabReport from './inventory/pages/Raw items/purchase raw lab report/PurchaseRawLabReport';
-import PurchaseRawRelease from './inventory/pages/Raw items/purchase raw release/PurchaseRawRelease';
-import PurchaseRawLotView from './inventory/pages/Raw items/purchase raw lot view/PurchaseRawLotView';
-import PurchaseRawPreview from './inventory/pages/Raw items/puchase raw preview/PurchaseRawPreview';
 
 
 
-import Navigation from './inventory/components/navigation/navigation';
+
 function App() {
   return (
     <div>
-      <Navigation/>
+      <h1>ryutjfhkgbknm, </h1>
+      
 
 
 
 
       <Router>
       <Routes>
-        <Route element={<Customer/>} path="/customer"/>
-        <Route element={<Supplier/>} path="/supplier"/>
-        <Route element={<CreateNewRawItem/>} path="/create/raw"/>
-        <Route element={<CreateNewProduct/>} path="/create/product"/>
-
-        <Route element={<PurchaseRaw/>} path="/purchase/raw"/>
-        <Route element={<PurchaseRawStore/>} path='/purchase/raw/store' />
-        <Route element={<PurchaseRawStore/>} path='/purchase/raw/store/:invoice_id' />
-        <Route element={<PurchaseRawLabReport/>} path='/purchase/raw/labreport'/>
-        <Route element={<PurchaseRawRelease/>} path="/purchase/raw/release"/>
-        <Route element={<PurchaseRawLotView/>} path="/purchase/raw/view"/>
-
-
-        <Route element={<PurchaseRawPreview/>} path="/purchase/raw/preview/:invoice_id"/>
-
+        {/* <Route element={<Customer/>} path="/customer"/> */}
+       
 
       </Routes>
 
